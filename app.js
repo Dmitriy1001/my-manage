fa3fd2c4e9b4c705f0e2c1861b5520da60864fa4
Dmitriy1001 <dumatkachuk@gmail.com>
2022-02-18 10:37:15 +0200

    finished users manage part of the website

--- app.js
+++ app.js
@@ -25,12 +25,21 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     if (req.session.signin) {
         let username = req.session.username;
-        connection.query('SELECT * FROM user;', async (error, results) => {
+        connection.query('SELECT * FROM user;', async (error, users) => {
             if (error) console.log(error);
-            else res.render('index', {sitename: 'MyManage', users: results, username: username});
+            else {
+                const user = users.filter(user => user.name === req.session.username)[0];
+                if (user.status === 'blocked') {res.redirect('logout');}
+
+                else {
+                    res.render('index', {sitename: 'MyManage', username: username, users: users});
+                }
+            }
+
         })
     }
-    else res.redirect('/signin')
+
+    else res.redirect('signin')
 });
 
 
@@ -40,7 +49,7 @@ app.get('/signin', (req, res) => {
         let name = req.session.name || '';
         req.session.successMsg = '';
         res.render('signin', {
-            sitename: 'MyManage', error: false, name: name, successMsg: successMsg
+            sitename: 'MyManage', error: false, name: name, successMsg: successMsg, page: 'signin'
         });
     }
     else res.redirect('/')
@@ -54,7 +63,8 @@ app.get('/signup', (req, res) => {
             errorMsg: false,
             name: '',
             email: '',
-            successMsg: ''
+            successMsg: '',
+            page: 'signup'
         });
     }
     else res.redirect('/')
@@ -70,23 +80,72 @@ app.get('/logout', (req, res) => {
 
 // request.method = post
 
+app.post('/index', (req, res) => {
+    if (req.session.signin) {
+        const usersID = typeof (req.body.userID) === 'string' ? [req.body.userID] : req.body.userID;
+        const btn = req.body.btn;
+        let sql;
+        let param;
+
+        if (btn === 'delete') {
+            sql = `DELETE FROM user WHERE ID IN (${usersID.map(user => user)})`;
+            param = '';
+            if (usersID.includes(String(req.session.userid))) {
+                req.session.destroy(() => {
+                });
+            }
+        } else {
+            sql = `UPDATE user set status = ? where id in (${usersID.map(user => user)})`;
+            param = btn;
+        }
+
+        connection.query(sql, param, async (error, results) => {
+            if (error) console.log(error);
+        })
+    }
+    res.redirect('/');
+})
+
+
 app.post('/signin', async(req, res) => {
     if (!req.session.signin) {
 
         const name = req.body.name;
         const password = req.body.password;
+
         if (name && password) {
-            connection.query('SELECT * FROM user WHERE name = ?', [name], async (error, results) => {
-                if (results.length === 0 || !(await bcryptjs.compare(password, results[0].password)))
+            connection.query(`SELECT * FROM user WHERE name = ?`, [name], async (error, results) => {
+                let user = results[0] || '';
+                if (!user || !(await bcryptjs.compare(password, user.password))) {
                     res.render('signin', {
                         sitename: 'MyManage',
                         error: true,
                         errorMsg: 'Wrong name or password',
                         name: name,
-                        successMsg: ''
+                        successMsg: '',
+                        page: 'signin'
                     });
+                }
+
+                else if (user.status === 'blocked') {
+                    res.render('signin', {
+                        sitename: 'MyManage',
+                        error: true,
+                        errorMsg: `User ${name} is blocked`,
+                        name: name,
+                        successMsg: '',
+                        page: 'signin'
+                    });
+                }
+
                 else {
+                    connection.query(
+                        `UPDATE user SET last_singin_date = CURRENT_TIMESTAMP() WHERE name = ?`,
+                        name,
+                        async (error, results) => {if (error) console.log(error);}
+                    );
                     req.session.username = results[0].name;
+                    req.session.userid = results[0].id;
                     req.session.signin = true;
                     res.redirect('/');
                 }
@@ -106,25 +165,29 @@ app.post('/signup', async (req, res) => {
         const password = req.body.password;
         let passwordHash = await bcryptjs.hash(password, 8);
 
-        let msg;
-        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {msg = 'Invalid email;\n'}
-        else {msg = ''}
+        let invalidEmail = false;
+        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {invalidEmail = true}
 
         connection.query(
             'INSERT INTO user SET ?',
             {name: name, email: email, password: passwordHash},
             async (error, results) => {
-                if (error || msg) {
-                    if (error.code === 'ER_DUP_ENTRY') {
-                        msg += 'There is already a user in the database with the same username and/or email';
+
+                if (invalidEmail || error) {
+                    let msg;
+                    if (invalidEmail) {msg = 'Invalid email';}
+                    else if (error && error.code === 'ER_DUP_ENTRY') {
+                        msg = 'There is already a user in the database with the same username and/or email';
                     }
                     res.render('signup', {
                         sitename: 'MyManage',
                         errorMsg: msg,
                         name: name,
-                        email: email
+                        email: email,
+                        page: 'signup'
                     });
                 }
+
                 else {
                     req.session.successMsg = 'You have successfully registered';
                     req.session.name = name;
@@ -133,6 +196,7 @@ app.post('/signup', async (req, res) => {
             })
     }
 });
+
 
 
 app.listen(3000, (req, res) => {
